e720cdace78b158452d37bb0b815994434951fad
Ratiq Narwal <ratiqnarwal@gmail.com>
2023-08-03 09:45:10 +0530

    rebase into gradle and tests for scanner and parser

---
+++ Rebased into gradle/craftinginterpreters/app/src/main/java/craftinginterpreters/trick/Interpreter.java
@@ -0,0 +1,160 @@
+package craftinginterpreters.trick;
+
+class Interpreter implements Expr.Visitor<Object>{
+    /*Public API connecting the expression interaction of Interpreter, Expr,
+        and Parser to the character consuming program of Trick
+    @param: expression - of Expr type
+    @return: void
+    */
+    void interpret(Expr expression){
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Trick.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object){
+        if(object == null) return "nil";
+
+        if(object instanceof Double){
+            String text = object.toString();
+            if(text.endsWith(".0")){
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+
+    /*Takes the subexpression of the other evaluating methods
+    and sends that subexpression to be parsed
+    @param: expression object/instance
+    @return: instance of Expr accept method
+     */
+    private Object evaluate(Expr expr){
+        return expr.accept(this);
+    }
+
+    /*Determines if any expression is inherently true or false
+     * @param: object of analysis
+     * @return: true/false
+     */
+    private boolean isTruthy(Object object){
+        if(object == null) return false;
+        if(object instanceof Boolean) return (boolean)object;
+        //anything else is automatically deemed true
+        return true;
+    }
+
+    /*Determines equality and checks for null objects
+     * @param: objects to compare
+     * @return: equal or not
+     */
+    private boolean isEqual(Object a, Object b){
+        if(a == null && b == null) return true;
+        if(a == null) return false;
+        //the equal method will satisfy our trick documentation
+        return a.equals(b);
+    }
+    /*Ensures the taken obejcts are actually number (Double) instances
+     * @param: operator Token, and operand object(s)
+     * @return: none
+     */
+    private void checkNumberOperand(Token operator, Object operand){
+        if(operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right){
+        if(left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Both operands must be a number.");
+    }
+
+    /*evaluates literals by returning the value
+     * @param: expression object belonging to literal subclass
+     * @return: literal value of the expression
+     */
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr){
+        return expr.value;
+    }
+
+    /*evaluates parentheses grouping by evaluating subexpression
+     * @param: expression object belonging to grouping subclass
+     * @return: instance of the evaluation of the subexpression
+     */
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr){
+        return evaluate(expr.expression);
+    }
+
+    /*evaluates unary cases
+     * @param: expression object belonging to unary subclass
+     * @return: instance of the evaluation of unary operator applied
+     * to evaluated subexpression
+     */
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr){
+        Object right = evaluate(expr.right);
+
+        switch(expr.operator.type){
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                checkNumberOperand(expr.operator,right);
+                return -(double)right;
+        }
+
+        //Unreachable for whatever reason - satisfying method return syntax
+        return null;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr){
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch(expr.operator.type){
+            case GREATER:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left >= (double)right;
+            case LESS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left <= (double)right;
+            case BANG_EQUAL: return !isEqual(left,right);
+            case EQUAL_EQUAL: return isEqual(left,right);
+            case MINUS:
+                checkNumberOperands(expr.operator, left, right);
+                return(double)left - (double)right;
+            case PLUS:
+                if(left instanceof Double && right instanceof Double){
+                    return (double)left + (double)right;
+                }
+                if(left instanceof String || right instanceof String){ //allows for things like hello + 4
+                    return stringify(left) + stringify(right);
+                }
+                if(left instanceof Character || right instanceof Character){
+                    return stringify(left) + stringify(right);
+                }
+
+            throw new RuntimeError(expr.operator, "Operands must be a string/number.");
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left / (double)right;
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left * (double)right;
+        }
+
+        //Unreachable as mentioned in visitUnaryExpr
+        return null;
+    }
+}
---
+++ Rebased into gradle/craftinginterpreters/app/src/main/java/craftinginterpreters/trick/Scanner.java
@@ -0,0 +1,365 @@
+package craftinginterpreters.trick;
+
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static craftinginterpreters.trick.TokenType.*;
+
+// Class where we scan through code and tokenize the source
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+    private static final Map<String,TokenType> keywords;
+
+    static{
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+
+    /*
+     * Construct will instantiate the source code with the provided source file for each scanner object
+     * @param source - Source file
+    */
+    Scanner(String source){
+        this.source = source;
+    }
+
+    /*
+     * Produceds a list of token classes rather than token objects, as we only want to instantiate
+     *  the objects when we need them in the moment thus saving memory in a current run time
+     * @param none
+     * @return list of token classes - list<class>
+     */
+    List<Token> scanTokens(){
+        while(!isAtEnd()){
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    /*
+     * Helper method to determine if we have reached the end of the reading file
+     * @param none
+     * @return Finished reading - boolean
+     */
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    /*
+     * The identifier of all the words to determine what we send to the tokenizer
+     * @param none
+     * @return none
+     */
+    private void scanToken(){
+        char c = advance();
+        switch(c){
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL: BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if(match('/')){
+                    while(peek() != '\n' && !isAtEnd()) advance();
+                }
+                else if(match('*')){
+                    advance();
+                    // while(peek() != '*' && peekNext() != '/' && !isAtEnd())  {
+                    while(!(peek() == '*' && peekNext() == '/' && !isAtEnd())) advance();
+                    advance();
+                    advance();
+                }
+                else{
+                    addToken(SLASH);
+                }
+                break;
+            //below skips over chars we dont care about: i.e whitespace
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            //begin more complex tokens such as strings
+            case '"': string(); break;
+
+            case '\'': character(); break;
+
+            default:
+                /* 
+                 * |************| This can be problematic as a blob of
+                 * random text will return error for each char of that blob
+                 * improve this by reporting blobs of unknown text as one error
+                 */
+                if(isDigit(c)){
+                    number();
+                }
+                else if(isAlpha(c)){
+                    identifier();
+                }
+                else{
+                    bloberror();
+                }
+                break;
+        }
+    }
+
+    /*
+     * Gives us the right char to tokenize moving along the code
+     * @param none
+     * @return the char to tokenize - char
+     */
+    private char advance(){
+        return source.charAt(current++);
+    }
+
+    /*
+     * Polymorphed method taking the simple commands of the swithc statement in method "scanToken" and understanding how to fill
+     * in the other parameters of tokenization
+     * @param type - the enum object of what kind of token we have
+     * @return none
+     */
+    private void addToken(TokenType type){
+        addToken(type,null);
+    }
+
+    /*
+     * Continuation of the polymorphed method setting the token object to be filled with the correct enum object type
+     * setting the Object to be null as we are not working with literals wit these single char tokens
+     * filling in the line number and what the actual string of the token is
+     * @param type -  token type
+     * @param literal - object type
+     * @return none
+     */
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type,text,literal,line));
+    }
+
+    /*
+     * Checks for more complex operations such as != and <= treating
+     *  them as a single token, telling us if we have matched such
+     * a case or are still within the simple cases
+     * @param exprected - char to check - char
+     * @return if we have matched a more complex case - boolean
+     */
+    private boolean match(char expected){
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    /*
+     * Lookahead method similar to match but here we will use this to continuelly check
+     * for the next character till we reach EOF or EOL, doesnt consume chars
+     * @param none
+     * @return identifying char of end of commentting system - char
+     */
+    private char peek(){
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    //STRING LITERALS
+
+    /*
+     * handles the tokenization parameters upon encountering a string
+     * @param none
+     * @return none
+     */
+    private void string(){
+        //this means we can have multi line strings, may want a different implementation
+        // |************|
+        while(peek() != '"' && !isAtEnd()){
+            // if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()){
+            Trick.error(line, "Unterminated string.");
+            return;
+        }
+
+        //advance past the closing "
+        advance();
+        //|*************| \n is not supported for strings - may want to implement this
+        //grab the string as object and send it as our object in the token class
+        String value = source.substring(start + 1, current-1);
+        addToken(STRING, value);
+    }
+
+    //CHAR LITERALS ?!?!?!?!
+
+    private void character(){
+        if(peek() != '\'' && !isAtEnd()){ // Case where there is a char and it is in the middle
+            advance();
+        }
+        else{
+            if(isAtEnd()){
+                Trick.error(line, "Unterminated char");     //Case where the char doesnt end
+            }
+            else{
+                Trick.error(line, "No expression given to char"); //Case where the char is empty
+                advance();
+            }
+            return;
+        }
+        if(!isAtEnd() && peek() != '\''){
+            while(peek() != '\'' && !isAtEnd()) advance();
+            if(isAtEnd()){
+                Trick.error(line, "Unterminated char");
+            }
+            else{
+                Trick.error(line, "Invalid char");
+                advance();
+            }
+            return;
+        }
+        else if(isAtEnd()){
+            Trick.error(line, "Unterminated char");
+            return;
+        }
+
+        String value = source.substring(start + 1 , current);
+        advance();
+        addToken(CHAR, value);
+    }
+
+    // NUMBER LITERALS
+
+    /*
+     * Checks if the char at hand is a digit value
+     * @param c - the present char in the source file - char
+     * @return  is it a digit or not - boolean
+     */
+    private boolean isDigit(char c){
+        return c >='0' && c <= '9';
+    }
+
+    /*
+     * Analyzes the whole number and any . parts to take the whole lexeme and send it for tokenization
+     * @param none
+     * @return none
+     */
+    private void number(){
+        while(isDigit(peek())) advance();
+
+        //fraction
+        if(peek() == '.' && isDigit(peekNext())){
+            advance();
+
+            while(isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    /*
+     * An further peek method, this is the most we will peek at a time - 2 chars ahead - and this seocndary function makes that clear
+     * @param none
+     * @return what is the char 2 steps ahead - char
+     */
+    private char peekNext(){
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    //RESEVED WORDS AND IDENTIFIERS
+
+    /*
+     * Collects the whole word while it is alphanumeric, and tokenizes it as keyword if the whole string is a keyword, else its an identifier
+     * @param none
+     * @return none
+     */
+    private void identifier(){
+        while(isAlphaNumeric(peek())) advance();
+        //Reserved Words Catcher
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    /*
+     * Is the char an alphabet character according to ASCII rules
+     * @param c - the char of analysis - char
+     * @return is it a valid char or not - boolean
+     */
+    private boolean isAlpha(char c){
+        return(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    /*
+     * is the char of analysis alphanumeric
+     * @param c - the char of analysis - char
+     * @return is it alphanumeric or not - boolean
+     */
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+
+    //BLOB ERROR REPORTING ?!?!?!?!?!
+
+    /*
+     * if an unrecognized text is presented, we keep reading the whole text as an error, rather than have "ajisdof"
+     * as an error for each single character we read
+     * @param none
+     * @return none
+     */
+    private void bloberror(){
+        while(peek() != ' ' && !isAtEnd()){
+            if(peek() == '\n'){
+                line++;
+                break;
+            }
+            advance();
+        }
+        Trick.error(line, "Unrecognized Text");
+    }
+
+}
